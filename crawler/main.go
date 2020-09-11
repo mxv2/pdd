2c95b54ed31536b59f5bbed9714dec192ef967bc
Maksim Kiryanov <mxvkiryanov@gmail.com>
2020-09-11 11:08:08 +0300

    Add flags - token, dir

--- crawler/main.go
+++ crawler/main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
 )
 
-const authToken = "remember_82e5d2c56bdd0811318f0cf078b78bfc=eyJpdiI6InZCSFNvOWJnOE1NUDJQRzNnbXhqdlE9PSIsInZhbHVlIjoiNzVEZnhpZHFlRXhDQ1wvNzFJZkNkQlVjM0xTMmUydG84OEd6THRJQkc3cWxvOHhqZHhoTU1RNjg5MHBTdkhUa1dvelhnVEI4eVBielp3VzkxRDhBcXRYUDJlWWU2RHhMaW50eklwVHpjSGc4PSIsIm1hYyI6ImI1OTYyNjUzYjA0OTNlZDZjNjY5M2NhNDcyMzkwYTEwMmZlODQ5OGJjMGYwZTUyMTM2M2QzZWQyYzc1ZjdlYzkifQ%3D%3D; quiz_605=1; current_block=18; XSRF-TOKEN=eyJpdiI6IjM3cVhuR1VkWmVVV214RGZJOVo0Q2c9PSIsInZhbHVlIjoiTGowcFg2cnRVblhtNkVlRVZUN2hNODhORXViQ09HZkVPMnVtalwvVmVmRGJwXC9tdGVwVXlZMG1zTnB5UklOSzBJakNsQ0VmNm9laGc1aHJ1ZmQxcmgzQT09IiwibWFjIjoiMGEzM2E3NGI5ZDY2ZGJkNTJjMWMxODE1NTg4YzI2YzI0MTRjNzY4NTUyOTdjYWNlODFjYWFkZGYwNWMxOTJjYyJ9;"
+var (
+	themeIDs = []int{130}
 
-var themeIDs = []int{130}
+	authToken string
+	workDir   string
+)
+
+func init() {
+	const (
+		defaultAuthToken = "remember_82e5d2c56bdd0811318f0cf078b78bfc=eyJpdiI6InZCSFNvOWJnOE1NUDJQRzNnbXhqdlE9PSIsInZhbHVlIjoiNzVEZnhpZHFlRXhDQ1wvNzFJZkNkQlVjM0xTMmUydG84OEd6THRJQkc3cWxvOHhqZHhoTU1RNjg5MHBTdkhUa1dvelhnVEI4eVBielp3VzkxRDhBcXRYUDJlWWU2RHhMaW50eklwVHpjSGc4PSIsIm1hYyI6ImI1OTYyNjUzYjA0OTNlZDZjNjY5M2NhNDcyMzkwYTEwMmZlODQ5OGJjMGYwZTUyMTM2M2QzZWQyYzc1ZjdlYzkifQ%3D%3D; XSRF-TOKEN=eyJpdiI6IjM3cVhuR1VkWmVVV214RGZJOVo0Q2c9PSIsInZhbHVlIjoiTGowcFg2cnRVblhtNkVlRVZUN2hNODhORXViQ09HZkVPMnVtalwvVmVmRGJwXC9tdGVwVXlZMG1zTnB5UklOSzBJakNsQ0VmNm9laGc1aHJ1ZmQxcmgzQT09IiwibWFjIjoiMGEzM2E3NGI5ZDY2ZGJkNTJjMWMxODE1NTg4YzI2YzI0MTRjNzY4NTUyOTdjYWNlODFjYWFkZGYwNWMxOTJjYyJ9;"
+	)
+	tmpDir := os.TempDir()
+	flag.StringVar(&authToken, "token", defaultAuthToken, "token for API client")
+	flag.StringVar(&workDir, "dir", tmpDir, "directory for storing data")
+}
 
 func main() {
+	flag.Parse()
+	log.Printf("Use token: %s", authToken)
+	log.Printf("Use work dir: %s", workDir)
+
 	client := newClient(authToken)
 
 	log.Println("Start crawling themes and questions")
